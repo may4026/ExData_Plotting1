e84751d660ff1243b2b8b3c92d284bfe5b00f960
may4026 <163556973+may4026@users.noreply.github.com>
2024-04-11 07:36:35 -0400

    Final plot3.R

--- plot3.R
+++ plot3.R
@@ -11,7 +11,9 @@ with(data1, {
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
 })
+
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
