8ba5e36e4f3a20f437678f0a2572ece4ea3af5c8
may4026 <163556973+may4026@users.noreply.github.com>
2024-04-11 07:38:12 -0400

    Final plot2.R

--- plot2.R
+++ plot2.R
@@ -9,5 +9,6 @@ with(data1, {
   plot(Global_active_power~Datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
 })
+
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
