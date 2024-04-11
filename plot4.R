d76f43f416df067c231636c1b9e26e5257459088
may4026 <163556973+may4026@users.noreply.github.com>
2024-04-11 07:37:13 -0400

    Final plot4.R

--- plot4.R
+++ plot4.R
@@ -6,6 +6,7 @@ datetime <- paste(as.Date(data1$Date), data1$Time)
 data1$Datetime <- as.POSIXct(datetime)
 
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+
 with(data1, {
   plot(Global_active_power~Datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
@@ -20,5 +21,6 @@ with(data1, {
   plot(Global_reactive_power~Datetime, type="l", 
        ylab="Global Rective Power (kilowatts)",xlab="")
 })
+
 dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
